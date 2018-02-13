4e92032df853453e83f7a4647c3322dff93c7508
ndxbxrme <lewis_the_cat@hotmail.com>
2018-02-13 08:30:43 +0000

    object sorting

--- build/index.js
+++ build/index.js
@@ -14,45 +14,79 @@
   module.factory('Sorter', function() {
     return {
       create: function(opts) {
-        return {
-          set: function(field) {
-            if (opts.sort !== field) {
-              opts.sort = field;
-              return opts.sortDir = 'ASC';
-            } else {
-              if (opts.sortDir === 'DESC') {
-                return opts.sortDir = 'ASC';
+        if (Object.prototype.toString.call(opts.sort) === '[object Object]') {
+          return {
+            set: function(field) {
+              var firstKey, i, key, outobj;
+              firstKey = Object.keys(opts.sort)[0];
+              outobj = {};
+              if (firstKey !== field) {
+                outobj[field] = 1;
               } else {
-                return opts.sortDir = 'DESC';
+                outobj[firstKey] = opts.sort[firstKey] === 1 ? -1 : 1;
               }
+              i = 0;
+              for (key in opts.sort) {
+                if (i++ > 0) {
+                  outobj[key] = opts.sort[key];
+                }
+              }
+              return opts.sort = outobj;
+            },
+            setLocal: function(field) {
+              return true;
+            },
+            class: function(field) {
+              var firstKey;
+              firstKey = Object.keys(opts.sort)[0];
+              return {
+                "has-sort": true,
+                sorting: firstKey === field,
+                desc: opts.sort[firstKey] !== 1
+              };
             }
-          },
-          setLocal: function(field) {
-            if (!opts.sort) {
-              opts.sort = '';
-            }
-            if (opts.sort.indexOf(field) === -1) {
-              opts.sort = field;
-              return opts.sortDir = 'ASC';
-            } else {
-              if (opts.sort.indexOf('-') === 0) {
+          };
+        } else {
+          return {
+            set: function(field) {
+              if (opts.sort !== field) {
                 opts.sort = field;
                 return opts.sortDir = 'ASC';
               } else {
-                opts.sort = `-${field}`;
-                return opts.sortDir = 'DESC';
+                if (opts.sortDir === 'DESC') {
+                  return opts.sortDir = 'ASC';
+                } else {
+                  return opts.sortDir = 'DESC';
+                }
               }
+            },
+            setLocal: function(field) {
+              if (!opts.sort) {
+                opts.sort = '';
+              }
+              if (opts.sort.indexOf(field) === -1) {
+                opts.sort = field;
+                return opts.sortDir = 'ASC';
+              } else {
+                if (opts.sort.indexOf('-') === 0) {
+                  opts.sort = field;
+                  return opts.sortDir = 'ASC';
+                } else {
+                  opts.sort = `-${field}`;
+                  return opts.sortDir = 'DESC';
+                }
+              }
+            },
+            class: function(field) {
+              var ref;
+              return {
+                "has-sort": true,
+                sorting: (-1 < (ref = opts.sort.indexOf(field)) && ref < 2),
+                desc: opts.sortDir === 'DESC'
+              };
             }
-          },
-          class: function(field) {
-            var ref;
-            return {
-              "has-sort": true,
-              sorting: (-1 < (ref = opts.sort.indexOf(field)) && ref < 2),
-              desc: opts.sortDir === 'DESC'
-            };
-          }
-        };
+          };
+        }
       }
     };
   });
--- src/index.coffee
+++ src/index.coffee
@@ -7,29 +7,50 @@ catch e
 
 module.factory 'Sorter', ->
   create: (opts) ->
-    set: (field) ->
-      if opts.sort isnt field
-        opts.sort = field
-        opts.sortDir = 'ASC'
-      else
-        if opts.sortDir is 'DESC'
-          opts.sortDir = 'ASC'
+    if Object.prototype.toString.call(opts.sort) is '[object Object]'
+      set: (field) ->
+        firstKey = Object.keys(opts.sort)[0]
+        outobj = {}
+        if firstKey isnt field
+          outobj[field] = 1
         else
-          opts.sortDir = 'DESC'
-    setLocal: (field) ->
-      if not opts.sort
-        opts.sort = ''
-      if opts.sort.indexOf(field) is -1
-        opts.sort = field
-        opts.sortDir = 'ASC'
-      else
-        if opts.sort.indexOf('-') is 0
+          outobj[firstKey] = if opts.sort[firstKey] is 1 then -1 else 1
+        i = 0
+        for key of opts.sort
+          if i++ > 0
+            outobj[key] = opts.sort[key]
+        opts.sort = outobj
+      setLocal: (field) ->
+        true
+      class: (field) ->
+        firstKey = Object.keys(opts.sort)[0]
+        "has-sort": true
+        sorting: firstKey is field
+        desc: opts.sort[firstKey] isnt 1
+    else
+      set: (field) ->
+        if opts.sort isnt field
           opts.sort = field
           opts.sortDir = 'ASC'
         else
-          opts.sort = "-#{field}"
-          opts.sortDir = 'DESC'
-    class: (field) ->
-      "has-sort": true
-      sorting: -1 < opts.sort.indexOf(field) < 2
-      desc: opts.sortDir is 'DESC'
+          if opts.sortDir is 'DESC'
+            opts.sortDir = 'ASC'
+          else
+            opts.sortDir = 'DESC'
+      setLocal: (field) ->
+        if not opts.sort
+          opts.sort = ''
+        if opts.sort.indexOf(field) is -1
+          opts.sort = field
+          opts.sortDir = 'ASC'
+        else
+          if opts.sort.indexOf('-') is 0
+            opts.sort = field
+            opts.sortDir = 'ASC'
+          else
+            opts.sort = "-#{field}"
+            opts.sortDir = 'DESC'
+      class: (field) ->
+        "has-sort": true
+        sorting: -1 < opts.sort.indexOf(field) < 2
+        desc: opts.sortDir is 'DESC'
